3e0149f7824783375eccd4381d43ddb2c23065e7
Hala Almohammadi <hala.almohammadi@kcl.ac.uk>
2023-10-03 03:00:30 +0100

    Add a method that returns a list of prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
+import math
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+     p = 2
+     
+    while len(list) < number_of_primes:
+        flag = True
+        for i in range(2, int(math.sqrt(p)) + 1):
+            if p%i==0:
+                flag = False
+                break    
+        if flag:
+            list.append(p)
+        p += 1
+        
     return list
